1d6f79e6b48de35805c1290616bd1b5c4243c6ab
atsybulskiy <atsibulskiy@gmail.com>
2018-08-23 00:25:45 +0300

    Common Chunk Plugin

--- src/profile.js
+++ src/profile.js
@@ -1,10 +1,5 @@
-import * as $ from 'jquery';
-
-
 import action from './common/action';
 import app from './common/app';
-
-console.log('$', $);
 
 action();
 app();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
+    entry: {
+        index: './index.js',
+        profile: './profile.js'
+    },
     output: {
-        filename: 'bundle.js',
+        //filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'development',
@@ -12,5 +18,44 @@ module.exports = {
         extensions: ['.js']
     },
     watch: false,
-    devtool: 'source-map'
+    devtool: 'source-map',
+
+    optimization: {
+        //minimize: true,
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: 'common',
+                    chunks: "initial",
+                    minChunks: 2,
+                    maxInitialRequests: 5, // The default limit is too small to showcase the effect
+                    minSize: 0 // This is example is too small to create commons chunks
+                },
+                vendor: {
+                    test: /node_modules/,
+                    chunks: "initial",
+                    name: "vendor",
+                    priority: 10,
+                    enforce: true
+                }
+            }
+        }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Learn JavaScript',
+            hash: true,
+            template: './index.html'
+        }),
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify('0.0.2'),
+            PRODUCTION: false,
+            HTML5_SUPPORT: true
+        }),
+        // new webpack.ProvidePlugin({
+        //     $: 'jquery'
+        // })
+
+    ]
 };
